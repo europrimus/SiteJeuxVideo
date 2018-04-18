ecc665d862435c313730ea03c2e1833cfee61f55
Didier Linux <europrimus@free.fr>
2018-04-18 14:53:23 +0200

    Structure du site
    Header, Footer

---
+++ site/include/footer.php
@@ -0,0 +1,9 @@
+	<footer>
+		<p>Page réalisé par : Melodie, Vincent, Julien, Didier et Luk</p>
+	</footer>
+
+</body>
+
+<!-- Les script JavaScript -->
+
+</html>
---
+++ site/include/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+<meta charset="utf-8">
+<title><?php getTitre(); ?></title>
+<meta name="generator" content="Geany 1.27">
+<!-- Feuille de style -->
+	<!--<link rel="stylesheet" href="css/style.css">-->
+
+<!-- Open Graph (http://ogp.me/) -->
+	<meta property="og:title" content="<?php getTitre(); ?>">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="">
+	<meta property="og:image" content="">
+</head>
+
+<body>
+	<header>
+		<h1><?php getTitre(); ?></h1>
+	</header>
