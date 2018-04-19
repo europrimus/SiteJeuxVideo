fdcae829a5048cba32c694300bfde4122c3b1295
Didier Linux <europrimus@free.fr>
2018-04-19 10:16:04 +0200

    Ajout d'un fichier de configuration
    Pour la base de données et le site
    renomer le fichier en config.php et editer les infos relatif au serveur

---
+++ site/include/config_defaut.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Configuration de la base de données
+ */
+
+/** Nom */
+define('DB_NAME', 'jeuxVideo');
+
+/** Utilisateur */
+define('DB_USER', '');
+
+/** Mot de passe */
+define('DB_PASSWORD', '');
+
+/** adresse */
+define('DB_HOST', 'localhost');
+
+/** encodage */
+define('DB_CHARSET', 'utf8_general_ci');
+
+
+?>
