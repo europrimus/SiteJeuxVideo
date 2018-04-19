2b152605f2fb81a4f562185f3f26ec19d08ab904
julien <julienbraga@hotmail.fr>
2018-04-19 10:56:05 +0200

    traiteemnt creation objeux

---
+++ site/formulaires/objeux.php
@@ -0,0 +1,33 @@
+<?php
+
+  
+  
+  
+  
+$donnees(
+_nom=>$_POST['titre'],
+_editeur=>$_POST['titre'],
+_support=>$_POST['titre'],
+_nbdl=>$_POST['titre'],
+date=>$_POST['date'],
+description=>$_POST['description']
+
+)
+
+  
+  
+require'jeu.php';
+
+
+$esaye= new jeu;
+
+
+
+
+
+echo esaye
+
+
+
+
+?>
