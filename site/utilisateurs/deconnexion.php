7a1b34525d2b52e5664ec6d4b6e7f8d3444e85fa
Luk <luk.f@live.fr>
2018-04-25 11:43:14 +0200

    Renvoi à l'acceuil en cas de deco

--- site/utilisateurs/deconnexion.php
+++ site/utilisateurs/deconnexion.php
@@ -9,8 +9,6 @@ include("includes/menu.php");
 if ($id==0) erreur(ERR_IS_NOT_CO);
 
 echo '<p>Vous êtes à présent déconnecté <br />
-Cliquez <a href="'.htmlspecialchars($_SERVER['HTTP_REFERER']).'">ici</a> 
-pour revenir à la page précédente.<br />
 Cliquez <a href="'.SITE["baseUrl"].'index.php">ici</a> pour revenir à la page principale</p>';
 echo '</div></body></html>';
 ?>
